ba947793e2e8a3312491c1e248dd0b76c7832881
Killian LACHAUX <lachauxk@iut-acy.local>
2018-01-19 14:36:02 +0100

    Premier pull du TP2

---
+++ ChannelMessaging/app/src/main/java/lachauxk/channelmessaging/LoginActivity.java
@@ -0,0 +1,20 @@
+package lachauxk.channelmessaging;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btnSubmit;
+    private EditText edxLogin;
+    private EditText edxPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btnSubmit = (Button) findViewById(R.id)
+    }
+}
